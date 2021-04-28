c72fe056938e12ee0d3ad1c788f6f76f1c22efde
FabioMonsores <fabio.dev.designer@gmail.com>
2021-04-28 17:03:43 -0300

    correção
    Organizando as pastas e transformando a primeira seção em JavaScript - Básico

--- Aula1 - Console_Log/exercicio.js
+++
@@ -1,8 +0,0 @@
-/*Escrever uma frase no console com o NOME entre aspas duplas, 
-um valor numério definido por VAR e finalizar a frase.*/
-
-// definindo a variável 
-var num = 10;
-
-// Apresentando no console a frase dita pelo professor mais valor numérico e finalizando com uma string.
-console.log('Me chamo "Fábio" e estou aprendendo JavaScript às', num, 'da manhã.');
--- Aula1 - Console_Log/index.js
+++
@@ -1,8 +0,0 @@
-console.log("Olá Mundo"); //string
-
-console.log(12345); //number, digita-se sem as aspas, o que chamamos de valor literal.
-
-console.log(12.8); //number
-
-var nome ="Fabio";
-console.log(nome);
