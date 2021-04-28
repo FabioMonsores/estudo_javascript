db84414bf845590a38cedb29994d6a2bc8668f25
FabioMonsores <fabio.dev.designer@gmail.com>
2021-04-28 16:51:43 -0300

    Update var_vs_let.js
    Diferenças entre let e var

--- Aula2 - Variáveis/var_vs_let.js
+++ Aula2 - Variáveis/var_vs_let.js
@@ -4,6 +4,3 @@ console.log(nome);
 // com VAR eu posso redeclarar uma variável, com LET não 
 
 nome = 'Lúcia'; // Isso criará uma variável global que pode afetar meu sistema... NÃO CRIAR SEM CONST OU LET.
-
-
-
