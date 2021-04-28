91850e5c24164a8e618925fc528d4aa0b23ab7e5
FabioMonsores <fabio.dev.designer@gmail.com>
2021-04-28 16:47:58 -0300

    Create var_vs_let.js

---
+++ Aula2 - Variáveis/var_vs_let.js
@@ -0,0 +1,9 @@
+var nome = 'Roberto';
+var nome = 'Fábio';
+console.log(nome);
+// com VAR eu posso redeclarar uma variável, com LET não 
+
+nome = 'Lúcia'; // Isso criará uma variável global que pode afetar meu sistema... NÃO CRIAR SEM CONST OU LET.
+
+
+
